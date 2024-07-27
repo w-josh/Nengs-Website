bb3549fee600653c3218975ffccf3ef918bb66af
w-josh <webj106@outlook.com>
2024-07-27 10:45:43 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mysql from "mysql2";
+
+const app = express();
+const port = 3000;
+
+const database = mysql.createConnection({
+    host: "localhost",
+    user: "lashviewer",
+    password:"lashes",
+    database: "lashdb",
+    port: 3302
+});
+
+database.connect((err) => {
+    if(err){
+        console.error("Error connecting to database: ", err);
+        return;
+    }
+    console.log("connected to database")
+})
+
+
+
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+    response.render("index.ejs");
+})
+
+app.listen(port, () => {
+console.log(`running on port: ${port}`);
+}) 
